b447d72f8d7b69fa630f7e66b167b2dd5ba33f5a
Erdal ASLAN <aslan.erdal.dev@gmail.com>
2024-12-17 22:55:35 +0100

    branche console-log

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 console.log("Je suis sur la  main");
 console.log("Je suis sur la  main 2");
+console.log("console sur la branche console-log");
+console.log("console sur la branche console-log 2");
